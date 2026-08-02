4767fd6993d9d7d46dd56f1a3e2c9cb2b36c3124
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2026-08-02 19:18:12 +0530

    improved votes page

--- docs/assets/js/feedback.js
+++ docs/assets/js/feedback.js
@@ -1,5 +1,4 @@
-function addEvents(idYes, idNo, permalink, feedbackdb) {
-	console.log(feedbackdb);
+function addVoteButtonEvents(idYes, idNo, permalink, feedbackdb) {
 	const buttonY = document.getElementById(idYes);
 	const buttonN = document.getElementById(idNo);
 
@@ -30,6 +29,7 @@ async function feedback(vote, permalink, feedbackdb) {
             reaction: vote
         })
     });
+	console.log(response);
 	if (response.ok) {
 		console.log("Okay");
         localStorage.setItem(`voted-${permalink}`, vote);
@@ -37,65 +37,55 @@ async function feedback(vote, permalink, feedbackdb) {
 			element.style.display = "none";
 		});
 		document.querySelector('#feedback span').innerHTML = "Your response has been recorded!";
-    }
+    } else {
+		console.log("Not Okay");
+		document.querySelector('#feedback span').innerHTML = "Oops, something seems to have gone wrong!";
+	}
 	console.log(localStorage.getItem(`voted-${permalink}`));
     console.log(await response.text());
 }
 
-async function loadFeedback() {
+async function loadVotes(type, feedbackdb, showId, hideId) {
+  const url = type == "detailed" ? `${feedbackdb}/api/feedback` : `${feedbackdb}/api/summary`
+  console.log(url, type);
   try {
-    const response = await fetch(
-      `$feedbackdb/api/feedback`
-    );
-
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`HTTP ${response.status}`);
     }
 
     const rows = await response.json();
-    const table = document.getElementById("votes");
+    const table = document.getElementById(showId);
 
-    table.innerHTML =
-      "<tr><th>Permalink</th><th>Reaction</th><th>Timestamp</th></tr>";
+	document.getElementById(showId).style.display = "block";
+	if (type == "detailed") {
+		table.innerHTML =
+		  "<tr><th>Permalink</th><th>Reaction</th><th>Timestamp</th><th>Region</th></tr>";
 
-    for (const row of rows) {
-      table.innerHTML += `
-        <tr>
-          <td>${row.permalink}</td>
-          <td>${row.reaction}</td>
-          <td>${row.timestamp}</td>
-        </tr>`;
-    }
-  } catch (err) {
-    console.error("Failed to load feedback:", err);
-  }
-}
+		for (const row of rows) {
+		  const dateObj = new Date(row.timestamp.replace(" ", "T") + "Z");;
+		  table.innerHTML += `
+			<tr>
+			  <td>${row.permalink}</td>
+			  <td>${row.reaction}</td>
+			  <td>${dateObj.toLocaleString()}</td>
+			  <td>${row.country} - ${row.region}</td>
+			</tr>`;
+		}
+	} else {
+		table.innerHTML =
+		  "<tr><th>Permalink</th><th>👍</th><th>👎</th></tr>";
 
-
-async function loadSummary() {
-  try {
-    const response = await fetch(
-      `$feedbackdb/api/summary`
-    );
-
-    if (!response.ok) {
-      throw new Error(`HTTP ${response.status}`);
-    }
-
-    const rows = await response.json();
-	const table = document.getElementById("votes");
-
-    table.innerHTML =
-      "<tr><th>Permalink</th><th>👍</th><th>👎</th></tr>";
-
-    for (const row of rows) {
-      table.innerHTML += `
-        <tr>
-          <td>${row.permalink}</td>
-          <td>${row.likes}</td>
-          <td>${row.dislikes}</td>
-        </tr>`;
-    }
+		for (const row of rows) {
+		  table.innerHTML += `
+			<tr>
+			  <td>${row.permalink}</td>
+			  <td>${row.likes}</td>
+			  <td>${row.dislikes}</td>
+			</tr>`;
+		}
+	}
+	document.getElementById(hideId).style.display = "none";
   } catch (err) {
     console.error("Failed to load feedback:", err);
   }
--- pages/feedback.md
+++ pages/feedback.md
@@ -1,9 +1,21 @@
 ---
-title: "Votes: Detailed"
-permalink: /votes-detailed/
+title: "Votes"
+permalink: /votes/
 ---
 
-<table id="votes" style="width: max-content;"></table>
+<button id="votes-summary-button" class="button">Summary</button>
+<button id="votes-detailed-button" class="button">Detailed</button>
+
+<table id="votes-summary" style="width: max-content;"></table>
+<table id="votes-detailed" style="width: max-content;"></table>
 
 <script src="/assets/js/feedback.js"></script>
-<script>loadFeedback()</script>
+<script>
+document.getElementById("votes-summary-button").addEventListener('click', () => {
+    loadVotes("summary", "{{ site.feedback-db }}", "votes-summary", "votes-detailed");
+});
+document.getElementById("votes-detailed-button").addEventListener('click', () => {
+    loadVotes("detailed", "{{ site.feedback-db }}", "votes-detailed", "votes-summary");
+});
+loadVotes("summary", "{{ site.feedback-db }}", "votes-summary", "votes-detailed")
+</script>
